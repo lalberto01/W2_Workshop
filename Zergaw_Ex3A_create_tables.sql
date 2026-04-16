3cf77e8dbc1a6d8ec6d5d86f2a4ef4654446a703
Tita9630 <hailutita@gmail.com>
2026-04-16 15:47:08 -0400

    Removed crete tables SQL file

--- Zergaw_Ex3A_create_tables.sql
+++
@@ -1,8 +0,0 @@
-/*
-The purpose of sakila database is to organize data, create queies.
-*/
-
-USE sakila;
-SELECT * FROM actor; -- Retrieved 200  records
-
-SELECT * FROM film; -- Retrieved 1000 records 
