e26592c2411896ece28bce58f83a9e4699e889f8
Tita9630 <hailutita@gmail.com>
2026-04-16 15:40:47 -0400

    Added EX3A_creat_tables.sql_and_EX3A._tables.xlsx

---
+++ Zergaw_Ex3A_create_tables.sql
@@ -0,0 +1,8 @@
+/*
+The purpose of sakila database is to organize data, create queies.
+*/
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200  records
+
+SELECT * FROM film; -- Retrieved 1000 records 
